cc57829f9951c80c747bb21aebdfce80a0767f60
Hayden Setlik <haydensetlik@gmail.com>
2023-06-25 18:27:48 -0600

    fixed clangd/tasks stuff

--- lua/hayden/core/keymaps.lua
+++ lua/hayden/core/keymaps.lua
@@ -19,7 +19,7 @@ keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available 
 
 -- neovim-tasks (mostly cmake stuff)
 keymap.set("n", "<leader>cg", "<cmd>Task start cmake_kits configure<cr>") -- configure current project
-keymap.set("n", "<leader>bm", "<cmd>Task set_module_param cmake_kits build_type<cr>") -- set build mode
+keymap.set("n", "<leader>cm", "<cmd>Task set_module_param cmake_kits build_type<cr>") -- set build mode
 keymap.set("n", "<leader>cx", "<cmd>Task cancel<cr>") -- cancel the current task
 keymap.set("n", "<leader>ct", "<cmd>Task set_module_param cmake_kits target<cr>") -- set build target
 keymap.set("n", "<leader>cr", "<cmd>Task start cmake_kits run<cr>") -- run current
@@ -31,3 +31,4 @@ keymap.set("n", "<leader>gb", "<cmd>Neogit branch<cr>") -- open the branch popup
 keymap.set("n", "<leader>gm", "<cmd>Neogit merge<cr>") -- open the merge popup
 keymap.set("n", "<leader>gp", "<cmd>Neogit pull<cr>") -- open the pull popup
 keymap.set("n", "<leader>gP", "<cmd>Neogit push<cr>") -- open the push popup
+keymap.set("n", "<leader>gs", "<cmd>Neogit <cr>") -- open the status popup
